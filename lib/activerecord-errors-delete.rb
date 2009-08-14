fca0608ad973dba79a19eba7efeb2f6dfdce59f3
Zach Dennis <zach.dennis@gmail.com>
2009-08-14 15:00:11 -0400

    Called #to_s when deleting an attribute to be consistent with how ActiveRecord::Errors#add works. This makes it possible to delete errors on attributes using a string or symbol

--- lib/activerecord-errors-delete.rb
+++ lib/activerecord-errors-delete.rb
@@ -3,7 +3,7 @@ class ActiveRecord::Errors
     raise "ActiveRecord::Errors already has a #delete method. You may be able to remove this plugin or something is getting loaded twice."
   else
     def delete(attr)
-      @errors.delete(attr)
+      @errors.delete(attr.to_s)
     end
   end
 end
